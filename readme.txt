186274cd561ce9ec7b4662e139b0fbbed6479e87
qpalz <qpalz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-25 00:54:57 +0000

    Fix last commit of version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/shortcode-in-comment/trunk@828260 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== Shortcode in Comment ===
+Contributors: Kelvin Ng
+Tags: shortcode,comments
+Requires at least: 3.1.0
+Tested up to: 3.8
+Stable tag: 1.0.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Enables blog owners to allow some shortcodes to be used in comments. You can choose what to allow.
+
+== Description ==
+This plugin enables blog owners to allow some shortcodes to be used in comments. You can choose what to allow (i.e. Whitelist some shortcodes). It only affects shortcodes in comments, but not in pages, posts, etc. All shortcodes are default to be disabled.
+
+= Settings =
+1. Visit 'Settings > Shortcode in Comment' to set what shortcodes are enabled in comments.
+2. Type your shortcode(s) to be enabled in the text box (comma seperated for more than one)
+3. Click 'Save Changes' to submit.
+
+== Installation ==
+= From Wordpress Dashboard =
+1. Visit 'Plugins > Add New'.
+2. Search for 'Shortcode in Comment'.
+3. Click 'Install Now'.
+4. Activate Shortcode in Comment from your Plugins page.
+5. Visit 'Settings > Shortcode in Comment' to set what shortcodes are enabled in comments.
+
+= Manually =
+1. Download the zip-compressed plugin.
+2. Extract and upload the folder 'shortcode-in-comment'.
+3. Activate Shortcode in Comment from your Plugins page.
+4. Visit 'Settings > Shortcode in Comment' to set what shortcode is enabled in comments.
+
+== Frequently Asked Questions ==
+= Will the plugin affect shortcodes in pages, posts, etc? =
+No. It only affects shortcodes in comments.
+
+== Screenshots ==
+1. Setting menu
+
+== Changelog ==
+= 1.0.0 =
+* Initial release.
+
+== Upgrade Notice ==
+= 1.0.0 =
+* Initial release.
---
+++ shortcode-in-comment.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Plugin Name: Shortcode in Comment
+ * Plugin URI: http://www.kelvinblog.tk/shortcode-in-comment
+ * Description: Allow users to use some shortcodes in comments
+ * Version: 1.0.0
+ * (function.bugfix.wordpress_update)
+ * Author: Kelvin Ng
+ * Author URI: http://www.kelvinblog.tk
+ * License: GPL3
+ */
+
+class shortcode_in_comment_options_page
+{
+	function __construct()
+	{
+		add_action('admin_menu', array($this, 'admin_menu'));
+	}
+
+	function admin_menu()
+	{
+		add_options_page('Enabled Shortcode in Comment', 'Shortcode in Comment', 'manage_options', 'shortcode_in_comment', array($this, 'settings_page'));
+	}
+
+	function settings_page()
+	{
+		if (isset($_POST['comment_enabled_shortcode']))
+		{
+			add_option('comment_enabled_shortcode');
+			update_option('comment_enabled_shortcode', $_POST['comment_enabled_shortcode']);
+		}
+?>
+<div class="wrap">
+<h2>Enabled Shortcode in Comment<h2>
+
+<form method="post" action=""> <!-- <form method="post"> can be used in HTML5, which is the standard --!>
+	<?php settings_fields('shortcode-in-comment-settings-group'); ?>
+	<?php do_settings_sections('shortcode-in-comment-settings-group'); ?>
+	<table class="form-table">
+		<tr valign="top">
+			<th scope="row">Enabled shortcode (comma seperated)</th>
+			<td><input type="text" name="comment_enabled_shortcode" value="<?php echo get_option('comment_enabled_shortcode'); ?>" /></td>
+		</tr>
+	</table>
+	
+	<?php submit_button(); ?>
+</form>
+</div>
+<?php
+	}
+}
+
+class shortcode_in_comment
+{
+	function __construct()
+	{
+		add_filter('comments_template', array($this, 'init_enabled_shortcodes'));
+	}
+
+	function init_enabled_shortcodes()
+	{
+		$enabled_shortcodes = get_option('comment_enabled_shortcode');
+		$enabled_shortcodes = explode(',', $enabled_shortcodes);
+
+		global $shortcode_tags;
+		foreach ($shortcode_tags as $tag => $func)
+		{
+			if (!in_array($tag, $enabled_shortcodes))
+			{
+				remove_shortcode($tag);
+			}
+		}
+
+		add_filter('comment_text', 'do_shortcode');
+	}
+}
+
+new shortcode_in_comment_options_page;
+new shortcode_in_comment;
+
+?>
